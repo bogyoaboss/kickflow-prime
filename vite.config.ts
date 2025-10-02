9d00d1812b7b168dcfad0b816c53b068f2d11d5a
bogyoaboss <tutuhd1@gmail.com>
2025-10-03 00:25:13 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: '/kickflow-prime/',
+  ...
+})
   server: {
     host: "::",
     port: 8080,
